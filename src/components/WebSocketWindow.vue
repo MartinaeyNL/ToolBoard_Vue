ab95f9bf9c549069e28a378f9ca5e8a125b64ae5
Martin Peeters <406336@student.fontys.nl>
2019-12-15 23:20:34 +0100

    WebSockets fully working

--- src/components/WebSocketWindow.vue
+++ src/components/WebSocketWindow.vue
@@ -2,13 +2,23 @@
   <div class="buttonBox">
     <span>Testjee</span>
     <v-btn @click="launchWebSocket">Connect WebSocket</v-btn>
+    <v-form>
+      <v-text-field v-model="txtInput" required label="Regular"></v-text-field>
+      <v-btn @click="sendMessage">Send Message</v-btn>
+    </v-form>
   </div>
 </template>
 
 <script>
 import websocket from "../logic/websocket.js";
-
 export default {
-  mixins: [websocket]
+  methods: {
+    launchWebSocket() {
+      websocket.methods.launchWebSocket();
+    },
+    sendMessage() {
+      websocket.methods.sendMessage(this.txtInput);
+    }
+  }
 };
 </script>
