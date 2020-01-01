ae1adab5c61089cc130ff88771b0aadee5a1e7f7
Martin Peeters <406336@student.fontys.nl>
2020-01-01 17:26:14 +0100

    Did some fixes with receiving messages and stuff

--- src/components/ComponentCards/Unique/ChatCard.vue
+++ src/components/ComponentCards/Unique/ChatCard.vue
@@ -2,16 +2,25 @@
   <div v-if="this.websocketErrorCheck() != null">
     <span>Connection closed with code [#{{ this.websocketError.code }}]</span>
   </div>
-  <div v-else-if="this.websocketIsNull() == true">Connecting to the server..</div>
-  <div v-else></div>
+  <div v-else-if="this.websocketIsNull() == true">
+    Connecting to the server..
+  </div>
+  <div v-else>
+    <h3>List of Chat Lobbies</h3>
+    <v-card v-for="lobby in this.chatLobbies" :key="lobby.displayname">
+      <span>{{ lobby.displayname }}</span>
+    </v-card>
+  </div>
 </template>
 
 <script>
 import websocket from "@/logic/websocket.js";
+
 export default {
   data: () => ({
     websocketError: null,
-    websocketCon: null
+    websocketCon: null,
+    chatLobbies: []
   }),
   created: function() {
     this.websocketCon = websocket.methods.launchWebSocket(
@@ -21,6 +30,11 @@ export default {
       this.$store,
       "setWebSocketChatError"
     );
+    this.websocketCon.onmessage = evt => {
+      // eslint-disable-next-line no-console
+      console.log(evt);
+      this.handleMessage(evt);
+    };
     this.$store.commit("setWebSocketChat", this.websocketCon);
   },
   methods: {
@@ -41,6 +55,13 @@ export default {
         return false;
       }
       return false;
+    },
+    handleMessage(evt) {
+      var result = JSON.parse(evt.data);
+      //switch(result.messageType) {
+      //  case
+      //}
+      alert(result);
     }
   }
 };
